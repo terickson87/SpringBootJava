c71c247db3abf939b223f2b1fdf808acf0564c36
Todd Ericskon <todd.erickson87@gmail.com>
2025-04-06 12:31:53 -0700

    add the spring initializer and built

---
+++ src/main/java/io/github/terickson87/cashcard/CashCardApplication.java
@@ -0,0 +1,13 @@
+package io.github.terickson87.cashcard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CashCardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CashCardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/terickson87/cashcard/CashCardApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.terickson87.cashcard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CashCardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
