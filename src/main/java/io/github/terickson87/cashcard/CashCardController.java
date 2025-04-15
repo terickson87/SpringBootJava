1d0730fcd30dfcab8ebeb57890333e09535b9f41
Todd Ericskon <todd.erickson87@gmail.com>
2025-04-15 15:36:50 -0700

    Added delete method

--- src/main/java/io/github/terickson87/cashcard/CashCardController.java
+++ src/main/java/io/github/terickson87/cashcard/CashCardController.java
@@ -64,6 +64,15 @@ class CashCardController {
         return ResponseEntity.notFound().build();
     }
 
+    @DeleteMapping("/{id}")
+    private ResponseEntity<Void> deleteCashCard(@PathVariable Long id, Principal principal) {
+        if (!cashCardRepository.existsByIdAndOwner(id, principal.getName())) {
+            return ResponseEntity.notFound().build();
+        }
+        cashCardRepository.deleteById(id);
+        return ResponseEntity.noContent().build();
+    }
+
     private CashCard findCashCard(Long requestedId, Principal principal) {
         return cashCardRepository.findByIdAndOwner(requestedId, principal.getName());
     }
