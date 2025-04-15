59855badbdfe025fb1fd6d8dc5907289bee2aaf1
Todd Ericskon <todd.erickson87@gmail.com>
2025-04-15 15:18:56 -0700

    added put

--- src/main/java/io/github/terickson87/cashcard/CashCardController.java
+++ src/main/java/io/github/terickson87/cashcard/CashCardController.java
@@ -53,4 +53,15 @@ class CashCardController {
             ));
         return ResponseEntity.ok(page.getContent());
     }
+
+    @PutMapping("/{requestedId}")
+    private ResponseEntity<Void> putCashCard(@PathVariable Long requestedId, @RequestBody CashCard cashCardUpdate, Principal principal) {
+        CashCard cashCard = cashCardRepository.findByIdAndOwner(requestedId, principal.getName());
+        if (cashCard != null) {
+            CashCard updatedCashCard = new CashCard(cashCard.id(), cashCardUpdate.amount(), principal.getName());
+            cashCardRepository.save(updatedCashCard);
+            return ResponseEntity.noContent().build();
+        }
+        return ResponseEntity.notFound().build();
+    }
 }
